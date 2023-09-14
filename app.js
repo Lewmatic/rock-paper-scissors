05cd06701e0121722a48e6436dbd63d8420f5933
Lewmatic <mwl2.2000@gmail.com>
2023-09-14 18:20:36 -0500

    Add function game() and update playRound()
    - The game() function orchestrates 5 rounds by calling playRound() iteratively.
    - Introduced global variables 'ties', 'playerWins', and 'computerWins' to track game statistics.
    - playRound() updated to increment the global variables based on the round's outcome.
    - game() is invoked in the main body of the code.

--- app.js
+++ app.js
@@ -35,29 +35,56 @@ function playRound(playerChoice, computerChoice) {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Tied`
     );
+    ties++;
   } else if (playerChoice == "rock" && computerChoice == "scissors") {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Win`
     );
+    playerWins++;
   } else if (playerChoice == "scissors" && computerChoice == "rock") {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Lose`
     );
+    computerWins++;
   } else if (playerChoice == "paper" && computerChoice == "rock") {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Win`
     );
+    playerWins++;
   } else if (playerChoice == "rock" && computerChoice == "paper") {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Lose`
     );
+    computerWins++;
   } else if (playerChoice == "scissors" && computerChoice == "paper") {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Win`
     );
+    playerWins++;
   } else if (playerChoice == "paper" && computerChoice == "scissors") {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Lose`
     );
+    computerWins++;
   }
 }
+
+function game() {
+  let numRounds = 1;
+  let playerChoice = "";
+  let computerChoice = "";
+  while (numRounds <= 5) {
+    playerChoice = getPlayerChoice();
+    computerChoice = getComputerChoice();
+    playRound(playerChoice, computerChoice);
+    numRounds++;
+  }
+  console.log(
+    `Final Score:\nPlayer Wins - ${playerWins}\nComputer Wins - ${computerWins}\nTies - ${ties}`
+  );
+}
+
+let ties = 0;
+let playerWins = 0;
+let computerWins = 0;
+game();
