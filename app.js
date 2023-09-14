326cee611f676d217df7cf0694a8be8c72d2c8e3
Lewmatic <mwl2.2000@gmail.com>
2023-09-14 17:36:58 -0500

    Added function playerSelection()
    - Validates user input for "rock", "paper", or "scissors".
    - Cleans input by removing whitespace and converting to lowercase.
    - Repeats input prompt until valid selection is entered.

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 function getComputerChoice() {
-    let rand = Math.floor(Math.random() * (3));
-    let computerChoice = ""
-    if (rand == 0) {
-        computerChoice += "rock";
-    }
-    else if (rand == 1) {
-        computerChoice += "paper";
-    }
-    else if (rand == 2) {
-        computerChoice += "scissors";
-    }
-    return computerChoice;
+  let rand = Math.floor(Math.random() * 3);
+  let computerChoice = "";
+  if (rand == 0) {
+    computerChoice += "rock";
+  } else if (rand == 1) {
+    computerChoice += "paper";
+  } else if (rand == 2) {
+    computerChoice += "scissors";
+  }
+  return computerChoice;
+}
+
+function playerSelection() {
+  userInput = prompt('Enter "rock", "paper", or "scissors": ');
+  userInput = userInput.replace(/\s/g, "");
+  userInput = userInput.toLowerCase();
+
+  while (
+    userInput != "rock" &&
+    userInput != "paper" &&
+    userInput != "scissors"
+  ) {
+    userInput = prompt(
+      'Invalid selection. Please enter either "rock", "paper", or "scissors": '
+    );
+    userInput = userInput.replace(/\s/g, "");
+    userInput = userInput.toLowerCase();
+  }
+  return playerSelection;
 }
