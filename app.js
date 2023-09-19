f9b049e6e4fd06ac3926cd89a3f10fdb9d3df979
Lewmatic <mwl2.2000@gmail.com>
2023-09-18 23:17:34 -0500

    Additions to UI, improved stylesheet
     - Added a score tracker to UI.
     - Added endGame() function which displays the winner after 5 rounds.
     - Renamed global variables (playerWins => wins, computerWins => losses)
       for readability.
     - Made changes to stylesheet for readability.

--- app.js
+++ app.js
@@ -21,46 +21,62 @@ function playRound() {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Win`
     );
-    playerWins++;
+    wins++;
   } else if (playerChoice == "scissors" && computerChoice == "rock") {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Lose`
     );
-    computerWins++;
+    losses++;
   } else if (playerChoice == "paper" && computerChoice == "rock") {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Win`
     );
-    playerWins++;
+    wins++;
   } else if (playerChoice == "rock" && computerChoice == "paper") {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Lose`
     );
-    computerWins++;
+    losses++;
   } else if (playerChoice == "scissors" && computerChoice == "paper") {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Win`
     );
-    playerWins++;
+    wins++;
   } else if (playerChoice == "paper" && computerChoice == "scissors") {
     console.log(
       `You chose ${playerChoice}\nComputer chose ${computerChoice}\nYou Lose`
     );
-    computerWins++;
+    losses++;
+  }
+  numRounds++;
+
+  if (numRounds === 5) {
+    endGame();
   }
 }
 
+function endGame() {
+  const results = document.getElementById("results");
+  if (wins > losses) {
+    results.textContent = "You Win!";
+  } else if (wins < losses) {
+    results.textContent = "You Lose :(";
+  } else results.textContent = "You Tied";
+  numRounds = 0;
+  wins = 0;
+  losses = 0;
+  ties = 0;
+}
+
 let playerChoice = "";
+let wins = 0;
+let losses = 0;
 let ties = 0;
-let playerWins = 0;
-let computerWins = 0;
 let numRounds = 0;
 
 const buttons = document.querySelectorAll("#cards button");
 buttons.forEach((button) =>
   button.addEventListener("click", () => {
-    numRounds++;
-    console.log(numRounds);
     button.classList.add("playing");
     button.addEventListener("transitionend", function (e) {
       if (e.propertyName !== "transform") return;
@@ -72,15 +88,26 @@ buttons.forEach((button) =>
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
+const score = document.getElementById("score");
+
 rock.addEventListener("click", () => {
   playerChoice = "rock";
   playRound();
+  if (ties > 0) {
+    score.textContent = `Score: W-${wins} / L-${losses} / T-${ties}`;
+  } else score.textContent = `Score: W-${wins} / L-${losses}`;
 });
 paper.addEventListener("click", () => {
   playerChoice = "paper";
   playRound();
+  if (ties > 0) {
+    score.textContent = `Score: W-${wins} / L-${losses} / T-${ties}`;
+  } else score.textContent = `Score: W-${wins} / L-${losses}`;
 });
 scissors.addEventListener("click", () => {
   playerChoice = "scissors";
   playRound();
+  if (ties > 0) {
+    score.textContent = `Score: W-${wins} / L-${losses} / T-${ties}`;
+  } else score.textContent = `Score: W-${wins} / L-${losses}`;
 });
